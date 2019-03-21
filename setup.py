41fbb912137811aac508525eb6317be460519bdc
Juan Infantes <jinfantes@google.com>
2019-03-21 16:12:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="vt_graph_api",
-    version="0.0.1",
+    version="1.0.0",
     author="VirusTotal",
     author_email="vt_graph_api@virustotal.com",
     description="VirusTotal Graph API",
