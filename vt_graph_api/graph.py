59792ff31ccb318342da8ff9c515acb19323cbb6
Alvaro Garcia <alvarogf@google.com>
2019-08-27 14:45:42 +0100

    debug

--- vt_graph_api/graph.py
+++ vt_graph_api/graph.py
@@ -624,11 +624,6 @@ class VTGraph(object):
     with concurrent.futures.ThreadPoolExecutor(
         max_workers=len(expansions)
     ) as expansion_pool:
-      self._log('safe')
-
-    with concurrent.futures.ThreadPoolExecutor(
-        max_workers=len(expansions)
-    ) as expansion_pool:
       has_quota = False
 
       if depth + 1 < max_depth:
@@ -653,38 +648,6 @@ class VTGraph(object):
           else:
             break
 
-        i = 0
-        while i < len(expansion_threads) and target_nodes:
-          nodes__, _ = expansion_threads[i].result()
-          expansion_type = expansions[i]
-
-          not_visited_nodes = (node for node in nodes__
-                               if node not in visited_nodes)
-          for node_ in not_visited_nodes:
-            lock.acquire()
-            if node_ in target_nodes:
-              path.append(
-                  (
-                      node.node_id,
-                      node_.node_id,
-                      expansions[i],
-                      node_.node_type
-                  )
-              )
-              solution_paths.append(path)
-              target_nodes.remove(node_)
-            else:
-              expansion_nodes.append(
-                  (
-                      node_,
-                      path + [(node.node_id,
-                               node_.node_id,
-                               expansion_type,
-                               node_.node_type)],
-                      depth + 1)
-                  )
-            lock.release()
-          i += 1
         self._log("terminate")
     self._log("at exit")
 
