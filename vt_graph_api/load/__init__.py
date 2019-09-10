745eea8cd97c29f7691690295f344549fa112209
Alvaro Garcia <alvarogf@google.com>
2019-09-10 10:02:06 +0100

    merging

--- vt_graph_api/load/__init__.py
+++ vt_graph_api/load/__init__.py
@@ -6,8 +6,8 @@ the user could load VTGraph in different ways.
 
 
 from vt_graph_api.load.maltego_csv import from_maltego_csv
-from vt_graph_api.load.maltego_xml import from_maltego_xml
+from vt_graph_api.load.maltego_graphml import from_maltego_graphml
 from vt_graph_api.load.vt_graph_id import from_vt_graph_id
 
 
-__all__ = ["from_vt_graph_id", "from_maltego_csv", "from_maltego_xml"]
+__all__ = ["from_vt_graph_id", "from_maltego_csv", "from_maltego_graphml"]
