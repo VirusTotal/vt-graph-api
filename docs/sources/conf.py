c97c5de76c1a42ab689343c3fb9cfc37576f57b4
Juan Infantes <jinfantes@google.com>
2019-11-04 15:30:21 +0100

    Fixing documentation and new release.

--- docs/sources/conf.py
+++ docs/sources/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, VirusTotal'
 author = 'VirusTotal'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
--- vt_graph_api/version.py
+++ vt_graph_api/version.py
@@ -4,5 +4,5 @@ This module provides version information.
 """
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __x_tool__ = 'Graph'
